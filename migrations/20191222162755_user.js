7ecf3a1304c718b0306c2cbff64be516bb58520f
edwardz8 <tolklorist@gmail.com>
2019-12-22 21:15:46 -0600

    add node server for db

---
+++ migrations/20191222162755_user.js
@@ -0,0 +1,13 @@
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('users', (table) => {
+        table.increments();
+        table.text('name').notNullable();
+        table.text('email').notNullable();
+        table.text('google_id').notNullable();
+        table.boolean('banned').notNullable().default(false);
+    })
+};
+
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTable('users');
+};
